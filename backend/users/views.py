d62d0d980177d056b087701f03b0d707634269c6
Gurin Valeriy <gurin.valerii@gmail.com>
2025-08-13 17:56:33 +0600

    develop 05commit Вместо 4. 4 не смотреть

---
+++ backend/users/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+
+from api.serializers import SubscriptionsSerializer
+
+
+User = get_user_model()
+
+
+class FgUserViewSet(UserViewSet):
+    http_method_names = ['get', 'post', 'put', 'delete']
+    pagination_class = LimitOffsetPagination
+
+
+    @action(['get',], url_path='subscriptions', detail=False)
+    def get_subscriptions(self, request):
+        return Response(
+            SubscriptionsSerializer(
+                request.user.subscriptions.all(),
+                many=True,
+                context={'request': request}
+            ).data,
+            status=status.HTTP_200_OK
+        )
+
+    @action(['put', 'delete'], url_path='me/avatar', detail=False)
+    def avatar(self, request, *args, **kwargs):
+        self.get_object = self.get_instance
+        if request.method == 'PUT':
+            return self.partial_update(request, *args, **kwargs)
+        elif request.method == 'DELETE':
+            request.user.avatar.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
