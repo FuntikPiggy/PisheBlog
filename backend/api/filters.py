73b2bdb5cbe4c1d8d715c113ff9d714fc773158b
Gurin Valeriy <gurin.valerii@gmail.com>
2025-08-29 20:03:08 +0600

    main 43commit All done

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters, BooleanFilter, NumberFilter
 
 from recipes.models import Recipe
 
@@ -10,17 +10,18 @@ User = get_user_model()
 class RecipeFilter(filters.FilterSet):
     """Фильтр для рецептов"""
 
-    tags = filters.AllValuesMultipleFilter(
-        field_name='tags__slug', lookup_expr='contains',)
-
-    is_favorited = filters.ModelChoiceFilter(
-        queryset=User.objects.all(), field_name='favorites__user_id',)
-    is_in_shopping_cart = filters.ModelChoiceFilter(
-        queryset=User.objects.all(), field_name='purchases__user_id',)
+    is_in_shopping_cart = NumberFilter(method='filter_is_in_shopping_cart')
+    is_favorited = NumberFilter(method='filter_is_favorited')
 
     class Meta:
         model = Recipe
         fields = ('author', 'is_favorited', 'is_in_shopping_cart',)
+
+    def filter_is_in_shopping_cart(self, queryset,):
+        return queryset.filter(purchases__user_id=self.request.user.id)
+
+    def filter_is_favorited(self, queryset,):
+        return queryset.filter(favorites__user_id=self.request.user.id)
 
 
 class IngredientFilter(filters.FilterSet):
